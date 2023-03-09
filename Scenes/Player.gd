5512e2e0d64a7344ebb034fd7591d143f3fef8a3
jaonewguy <3062330+jaonewguy@users.noreply.github.com>
2023-03-08 23:26:04 -0800

    Unimpl grass effect. Added attack and move anim

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,22 +1,45 @@
 extends KinematicBody2D
 
-var speed = 200
+var SPEED = 80
+var ATTACKS = ["Attack1", "Attack2"]
+
+var state_machine
 var velocity = Vector2.ZERO
+
+func _ready() -> void:
+    state_machine = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta: float) -> void:
     get_input()
     velocity = move_and_slide(velocity)
 
 func get_input() -> void:
+#    var current = state_machine.get_current_node()
     velocity = Vector2.ZERO
-    if Input.is_action_pressed('right'):
+    if Input.is_action_just_pressed("attack"):
+        state_machine.travel(ATTACKS[randi() % 2])
+        return
+    if Input.is_action_pressed("right"):
         velocity.x += 1
-    if Input.is_action_pressed('left'):
+        $Sprite.scale.x = 1
+    if Input.is_action_pressed("left"):
         velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
+        $Sprite.scale.x = -1
+    if Input.is_action_pressed("up"):
         velocity.y -= 1
-    # Make sure diagonal movement isn't faster
-    velocity = velocity.normalized() * speed
+    if Input.is_action_pressed("down"):
+        velocity.y += 1
+        
+    velocity = velocity.normalized() * SPEED
     
+    if velocity.length() != 0:
+        state_machine.travel("Run")
+    if velocity.length() == 0:
+        state_machine.travel("Idle")
+    
+#func hurt():
+#    state_machine.travel("hurt")
+#
+#func die():
+#    state_machine.travel("die")
+#    set_physics_process(false)
--- Scenes/TallGrass.gd
+++ Scenes/TallGrass.gd
@@ -1,15 +1,34 @@
 extends Node2D
 
 onready var anim_player = $AnimationPlayer
+const grass_overlay_texture = preload("res://Assets/Grass/stepped_tall_grass.png")
+const GrassStepEffect = preload("res://Scenes/GrassStepEffect.tscn")
+var grass_overlay: TextureRect = null
+
+var player_inside: bool = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#    pass
+#func _ready():
+#    var player = get_node("/root/SceneManager/CurrentScene").get_children().back().find_node("Player")
+#    player.connect("player_moving_signal", self, "player_exiting_grass")
+#    player.connect("player_stopped_signal", self, "player_in_grass")
+#
+#func player_exiting_grass():
+#    player_inside = false
+#    if is_instance_valid(grass_overlay):
+#        grass_overlay.queue_free()
+#
+#func player_in_grass():
+#    if player_inside == true:
+#        var grass_step_effect = GrassStepEffect.instance()
+#        grass_step_effect.position = position
+#        get_tree().current_scene.add_child(grass_step_effect)
+#
+#
+#        grass_overlay = TextureRect.new()
+#        grass_overlay.texture = grass_overlay_texture
+#        grass_overlay.rect_position = position
+#        get_tree().current_scene.add_child(grass_overlay)
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
